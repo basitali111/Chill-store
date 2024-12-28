4fda8bb3958206a5e3f7a8ff24d90b50440c7d38
basitali111 <allahbadshah148@gmail.com>
2024-12-28 15:14:08 +0500

    add code

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,11 +1,41 @@
-import mongoose from 'mongoose'
+// lib/dbConnect.ts
+import mongoose from 'mongoose';
 
-async function dbConnect() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI!)
-  } catch (error) {
-    throw new Error('Connection failed!')
-  }
+const { MONGODB_URI } = process.env;
+
+// Define MONGODB_URI as string after ensuring it's not undefined
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
 }
 
-export default dbConnect
+// At this point, TypeScript can infer MONGODB_URI is string
+const uri: string = MONGODB_URI;
+
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
